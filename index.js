9ac7239da456b3e584d92854b223b43ebda6d235
Krishna Prasanth.S <107831109+krishnaprasanth7102@users.noreply.github.com>
2022-07-18 17:11:54 +1030

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 "use strict";
-
 var input = document.getElementById('input'), // input/output button
   number = document.querySelectorAll('.numbers div'), // number buttons
   operator = document.querySelectorAll('.operators div'), // operator buttons
   result = document.getElementById('result'), // equal button
   clear = document.getElementById('clear'), // clear button
   resultDisplayed = false; // flag to keep an eye on what output is displayed
-
 // adding click handlers to number buttons
 for (var i = 0; i < number.length; i++) {
   number[i].addEventListener("click", function(e) {
-
     // storing current input string and its last character in variables - used later
     var currentString = input.innerHTML;
     var lastChar = currentString[currentString.length - 1];
